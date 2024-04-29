c5bd8225587e63a9d6f2edcc9e34dd9348e36b35
Ануар Курмангалиев <kurative.studio@gmail.com>
2024-04-29 21:31:09 +0500

    v1.0

---
+++ Assets/Scripts/Ads/AdsInitializer.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdsInitializer : MonoBehaviour, IUnityAdsInitializationListener
+{
+    [SerializeField] private string _androidGameId;
+    [SerializeField] private string _iOSGameId;
+    [SerializeField] private bool _testMode;
+    private string _gameId;
+
+    private void Awake()
+    {
+        if (Advertisement.isInitialized)
+            return;
+        InitializeAds();
+    }
+    public void InitializeAds()
+    {
+        _gameId = (Application.platform == RuntimePlatform.IPhonePlayer) ? _iOSGameId : _androidGameId;
+        Advertisement.Initialize(_gameId, _testMode, this);
+    }
+    public void OnInitializationComplete()
+    {
+        Debug.Log("Unity Ads initialization complete");
+    }
+
+    public void OnInitializationFailed(UnityAdsInitializationError error, string message)
+    {
+        Debug.Log($"Unity Ads initialization Failed: {error.ToString()} - {message}");
+    }
+}
